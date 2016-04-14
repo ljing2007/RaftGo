64a1c868adc032ebc0612944d76e6892b975469e
runshenzhu <runshen.zhu@gmail.com>
2016-04-14 16:10:56 -0400

    handle worker failure

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -24,20 +24,24 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	signals := make(chan bool, ntasks)
 	for i:=0; i < ntasks; i++{
+
+		// must save i to a local variable
 		taskIdx := i
 		go func(){
-			// get a free worker
-			worker := <- mr.registerChannel
-
-			// rpc call to finish job
-			args := DoTaskArgs{mr.jobName, mr.files[taskIdx], phase, taskIdx, nios}
-			ok := call(worker, "Worker.DoTask", &args, new(struct{}))
-			if !ok {
-				fmt.Printf("fail: %v %v tasks (%d I/Os)\n", taskIdx, phase, nios)
-			}else{
-				signals <- true
-				// return the free worke
-				mr.registerChannel <- worker
+			ok := false
+			for !ok {
+				// get a free worker
+				worker := <-mr.registerChannel
+				// rpc call to finish job
+				args := DoTaskArgs{mr.jobName, mr.files[taskIdx], phase, taskIdx, nios}
+				ok = call(worker, "Worker.DoTask", &args, new(struct{}))
+				if !ok {
+					fmt.Printf("fail: %v %v tasks (%d I/Os)\n", taskIdx, phase, nios)
+				}else {
+					signals <- true
+					// return the free worke
+					mr.registerChannel <- worker
+				}
 			}
 		}()
 	}
